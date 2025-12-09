aae775a0d31a525c6b99933ef35ded1cb3189ee6
Luis Adrian Ramos <luchoramos042004@gmail.com>
2025-12-09 17:56:57 -0500

    Initial commit

---
+++ lib/core/error/exceptions.dart
@@ -0,0 +1,16 @@
+class ServerExtencion implements Exception {
+  final String message;
+  final int?  statusCode;
+  const ServerExtencion({required this.message, this.statusCode});
+}
+
+class AuthException implements Exception {
+  final String message;
+  final String? code;
+  const AuthException({required this.message, this.code});
+}
+
+class CacheException implements Exception {
+  final String message;
+  const CacheException({required this.message});
+}
